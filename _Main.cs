ac105d21c679a1eb48e19d5645f534111b8a51b1
Ash <noreply@ash.dudmail.com>
2023-01-03 23:16:55 +0800

    Removed unused CMU loader line.

--- _Main.cs
+++ _Main.cs
@@ -29,7 +29,6 @@ namespace AphoBoundaryOfDeathAlts
             LocalizeUtil.RemoveError();
             CardUtil.InitKeywordsList(new List<Assembly> { Assembly.GetExecutingAssembly() });
             ArtUtil.InitCustomEffects(new List<Assembly> { Assembly.GetExecutingAssembly() });
-            CustomMapHandler.ModResources.CacheInit.InitCustomMapFiles(Assembly.GetExecutingAssembly());
         }
         private static void OnInitParameters()
         {
